0b1f7ab7711fec63179f455f12220eef051436b2
sahilrewale-09 <rewalesahil9@gmail.com>
2026-03-28 16:34:59 +0530

    stashing

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,1 +1,21 @@
-//token verification
+
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+    // Get token from the header
+    const token = req.header('x-auth-token');
+
+    // Check if no token exists
+    if (!token) {
+        return res.status(401).json({ message: 'No token, authorization denied' });
+    }
+
+    // Verify token
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user; // Adds user ID and Role to the request object
+        next(); // Move on to the next function (e.g., accessing the dashboard)
+    } catch (err) {
+        res.status(401).json({ message: 'Token is not valid' });
+    }
+};
